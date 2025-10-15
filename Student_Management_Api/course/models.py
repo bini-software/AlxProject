2c0bd718ddfce6c2f2a61c06b994bd7d436387fb
Biniyam Girma <biniswengineer@gmail.com>
2025-10-15 13:32:12 +0300

    Fix __str__ methods and add unique constraints in models

--- Student_Management_Api/course/models.py
+++ Student_Management_Api/course/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     title = models.CharField(max_length=150)
-    course_code = models.CharField(max_length=100)
+    course_code = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def str(self):
-        return f"{self.title}"
+    def __str__(self):
+        return f"{self.title} ({self.course_code})"
     
--- Student_Management_Api/student/models.py
+++ Student_Management_Api/student/models.py
@@ -5,9 +5,9 @@ class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     student_id = models.CharField(max_length=50, unique=True)
-    age = models.IntegerField()
+    age = models.DateField()
     email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def str(self):
+    def __str__(self):
         return f"{self.first_name} {self.last_name}"
