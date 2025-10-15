1bc5e1b3bf625d7910ad3420e7746d8cbf529fe2
Biniyam Girma <biniswengineer@gmail.com>
2025-10-15 13:00:40 +0300

    Remove api app from models-setup branch and push student, course, and enrollment apps again

--- Student_Management_Api/Student_Management_Api/urls.py
+++ Student_Management_Api/Student_Management_Api/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('api/', include('api.urls')),
     path('student/', include('student.urls')),
     path('course/', include('course.urls')),
     path('enrollment/', include('enrollment.urls')),
